e13a0b408d8edd260889a404a872b5c7fabfa5df
rsraj9980 <91348564+rsraj9980@users.noreply.github.com>
2021-11-02 19:13:04 -0400

    Add One-to-Many realtionship between Finch and Feeding

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
 
     path('finchs/', views.finchs_index, name='index'),
     path('finchs/<int:finch_id>', views.finchs_detail, name='detail'),
+
+    # new route used to show a form and create a finch 
+    path('finchs/create/', views.FinchCreate.as_view(), name='finchs_create'),
+
+    # new route used to update and delete a finch
+    path('finchs/<int:pk>/update/', views.FinchUpdate.as_view(), name ='finchs_update'),
+    path('finchs/<int:pk>/delete/', views.FinchDelete.as_view(), name ='finchs_delete'),
+    path('finchs/<int:finch_id>/add_feeding/', views.add_feeding, name ='add_feeding')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
+from django.db.models import fields
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 # Create your views here.
-
+from .forms import FeedingForm
 
 def home(request):
     return render(request, 'home.html')
@@ -17,4 +18,33 @@ def finchs_index(request):
 
 def finchs_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finchs/detail.html', { 'finch': finch })
+  # instantiate FeedingForm to be rendered in the template
+  feeding_form = FeedingForm()
+  return render(request, 'finchs/detail.html', {
+    'finch': finch, 'feeding_form': feeding_form
+  })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__' 
+  success_url = '/finchs/'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finchs/'
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the finch_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
